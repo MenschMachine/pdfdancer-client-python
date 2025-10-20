4abc80bf1d037c3e6b5f4fb9e71e6a9f24e03efc
Michael Lahr <michael.lahr@gmail.com>
2025-10-20 16:28:56 +0800

    ci: Update CI workflow to use HTTPS for API URL and set default base URL in tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 import os
+
 import pytest
 
 
 @pytest.fixture(scope="session", autouse=True)
 def set_test_environment():
     """Automatically set PDFDANCER_BASE_URL to localhost for all tests"""
-    os.environ["PDFDANCER_BASE_URL"] = "http://localhost:8080"
+    if os.getenv('PDFDANCER_BASE_URL') is None:
+        os.environ["PDFDANCER_BASE_URL"] = "http://localhost:8080"
     yield

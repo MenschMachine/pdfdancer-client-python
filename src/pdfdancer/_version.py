8f51df18ac16bf5d64e5185ac0aa39b7bfcca382
Michael Lahr <michael.lahr@gmail.com>
2026-04-03 13:47:34 +0800

    feat(release): update version
    Remove development version suffix and commit id to prepare for release

--- src/pdfdancer/_version.py
+++ src/pdfdancer/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.3.14.dev3+g7a57e804c.d20260403'
-__version_tuple__ = version_tuple = (0, 3, 14, 'dev3', 'g7a57e804c.d20260403')
+__version__ = version = '0.3.14'
+__version_tuple__ = version_tuple = (0, 3, 14)
 
-__commit_id__ = commit_id = 'g7a57e804c'
+__commit_id__ = commit_id = None
